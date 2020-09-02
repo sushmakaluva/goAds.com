72b71526f2bc93ec33fa908a485eb698f4ceb49c
Sushma Kaluva <kaluvasushma@gmail.com>
2020-09-02 12:30:25 -0400

    added gitignore,.env,npm libraries

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+const db = require('./models');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+require('./routes/api-routes.js')(app);
+require('./routes/html-routes.js')(app);
+
+db.sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`);
+  });
+});
