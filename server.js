9cfd1c4c61e902240b54a6cfd79225f40d0b32bf
Sushma Kaluva <kaluvasushma@gmail.com>
2020-09-02 13:00:42 -0400

    added sequelize - config.json , models folder/index.js files

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers/<>.js');
+const db = require('./models');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const db = require('./models');
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static('public'));
+app.use(routes);
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 require('./routes/api-routes.js')(app);
 require('./routes/html-routes.js')(app);
