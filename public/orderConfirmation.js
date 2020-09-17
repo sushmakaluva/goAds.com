69570f04339d358d5db9a36c229200bc249b55e7
Faduma Abdillahi <faduma92@gmail.com>
2020-09-17 00:27:15 -0400

    fixed redirecting page

--- public/orderConfirmation.js
+++ public/orderConfirmation.js
@@ -6,11 +6,12 @@ console.log(moment.locale()); // cs
 const firstName = $("#firstName").val()
 
 
+
 const lastName = $("#lastName").val()
 
-let date = moment().format("MMM Do YY");
+let date = moment().format("MMM Do YYYY");
 $("#Date").text(":  " + date)
-
+console.log(date)
 
 const Total = $("#grandTotal").val()
 
@@ -23,4 +24,4 @@ const country = $("#country").val()
 const state = $("#state").val()
 const zip = $("#zip").val()
 
-console.log(firstName, lastName, date, address, country, state, zip)
+console.log(firstName)
