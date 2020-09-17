7fe5c20b7f078fedd748eb8c89d8ba79ecf9724f
Kashyap Aghera <kaagh.10@gmail.com>
2020-09-17 16:33:13 -0400

    backend login authentication added!

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,58 +1,89 @@
-const Sequelize = require('sequelize');
-const db = require('../models');
+const Sequelize = require("sequelize");
+const db = require("../models");
 
 module.exports = function (app) {
   // Get route for returning products which have deals
-  app.get('/deals', (req, res) => {
+  app.get("/deals", (req, res) => {
     db.Products.findAll({
-      order: Sequelize.literal('rand()'), limit: 4,
-    })
-      .then((dbProduct) => {
-        const result = gridDisplay(dbProduct);
-        res.render('products', { products: result });
-      });
+      order: Sequelize.literal("rand()"),
+      limit: 5,
+    }).then((dbProduct) => {
+      console.log(dbProduct);
+      // res.json(dbProduct);
+      res.render("deals", { dbProduct });
+    });
   });
 
-  // GET route for retrieving a single product (not used)
-  app.get('/api/products/:product_id', (req, res) => {
+  // GET route for retrieving a single product
+  app.get("/api/products/:product_id", (req, res) => {
     db.Products.findOne({
       where: {
         product_id: req.params.product_id,
       },
-    })
-      .then((dbProduct) => {
-        res.json(dbProduct);
-      });
+    }).then((dbProduct) => {
+      res.json(dbProduct);
+    });
   });
 
   // POST route for saving a new product
-  app.post('/api/sell', (req, res) => {
+  app.post("/api/sell", (req, res) => {
     console.log(req.body);
     db.Products.create({
       product_name: req.body.product_name,
       price: req.body.price,
       category: req.body.category,
       description: req.body.description,
-    })
-      .then((dbProduct) => {
-        res.json(dbProduct);
-      });
+    }).then((dbProduct) => {
+      res.json(dbProduct);
+    });
   });
 
-  // DELETE route for deleting products from cart
-  app.delete('/api/products/:product_id', (req, res) => {
-    db.Products.destroy({
+  // POST route for ADD TO CART
+  app.post("/api/cart", (req, res) => {
+    console.log(req.body);
+    db.Cart.create({
+      product_name: req.body.product_name,
+      price: req.body.price,
+    }).then((dbCart) => {
+      res.json(dbCart);
+    });
+  });
+
+  // DELETE route for deleting PRODUCTS FROM CART
+  app.delete("/api/cart/:cart_id", (req, res) => {
+    db.Cart.destroy({
       where: {
-        product_id: req.params.product_id,
+        cart_id: req.params.cart_id,
       },
     })
-      .then((dbProduct) => {
-        res.json(dbProduct);
-      });
+      .then((dbCart) => {
+        res.json(dbCart);
+      })
+      .catch((error) => res.json(error));
   });
 
+  // GET ROUTE FOR DISPLAYING ALL ITEMS IN CART
+  app.get("/cart", (req, res) => {
+    db.Cart.findAll({ raw: true }).then((dbCart) => {
+      console.log(dbCart[0]);
+      res.render("cart", { cartItems: dbCart });
+    });
+  });
+
+  //   // FIND ONE PRODUCT IN CART
+  //   app.get('/api/cart/:product_id', (req, res) => {
+  //     db.Cart.findOne({
+  //       where: {
+  //         product_id: req.params.product_id,
+  //       },
+  //     })
+  //       .then((dbCart) => {
+  //         res.json(db);
+  //       });
+  //   });
+
   // GET route to display all the products based on category
-  app.get('/products', (req, res) => {
+  app.get("/products", (req, res) => {
     const whereCondition = {};
     if (req.query.category) {
       whereCondition.category = req.query.category;
@@ -60,67 +91,55 @@ module.exports = function (app) {
 
     db.Products.findAll({
       where: whereCondition,
-    })
-      .then((dbProducts) => {
-        const result = gridDisplay(dbProducts);
-        res.render('products', { products: result });
-      });
-  });
-
-  function gridDisplay(dbProducts) {
-    const result = [];
-    let temp = [];
-    for (let i = 0; i < dbProducts.length; i++) {
-      temp.push(dbProducts[i]);
-      if (temp.length === 3) {
-        result.push(temp);
-        temp = [];
+    }).then((dbProducts) => {
+      const result = [];
+      let temp = [];
+      for (let i = 0; i < dbProducts.length; i++) {
+        temp.push(dbProducts[i]);
+        if (temp.length === 3) {
+          result.push(temp);
+          temp = [];
+        }
       }
-    }
-    if (temp.length > 0) {
-      result.push(temp);
-    }
-    return result;
-  }
+      if (temp.length > 0) {
+        result.push(temp);
+      }
+      res.render("products", { products: result });
+    });
+  });
 
   // GET route to display recent Ads
-  app.get('/api/recent-ads', (req, res) => {
+  app.get("/api/recent-ads", (req, res) => {
     db.Products.findAll({
       limit: 3,
-      order: [['product_id', 'DESC']],
-    })
-      .then((dbProduct) => {
-        res.json(dbProduct);
-      });
+      order: [["product_id", "DESC"]],
+    }).then((dbProduct) => {
+      res.json(dbProduct);
+    });
   });
 
-  // GET route for search keyword
-  app.get('/search', (req, res) => {
-    console.log(req.query);
+  // GET route
+  app.get("/search", (req, res) => {
     db.Products.findAll({
       limit: 10,
       where: {
         product_name: {
-          [Sequelize.Op.like]: `%${req.query.keyword}%`,
+          [Sequelize.Op.like]: `%${req.body.query}%`,
         },
       },
-    })
-      .then((dbProduct) => {
-        const result = gridDisplay(dbProduct);
-        res.render('products', { products: result });
-      });
+    }).then((dbProduct) => {
+      res.json(dbProduct);
+    });
   });
 
   // PUT route for updating products in cart
-  app.put('/api/products', (req, res) => {
-    db.Products.update(req.body,
-      {
-        where: {
-          product_id: req.body.product_id,
-        },
-      })
-      .then((dbProduct) => {
-        res.json(dbProduct);
-      });
+  app.put("/api/products", (req, res) => {
+    db.Products.update(req.body, {
+      where: {
+        product_id: req.body.product_id,
+      },
+    }).then((dbProduct) => {
+      res.json(dbProduct);
+    });
   });
 };
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,21 +1,40 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = function (app) {
   // index route loads view.html
-  app.get('/', (req, res) => {
-    res.render('index');
+  app.get("/", (req, res) => {
+    res.render("index");
   });
 
-  app.get('/subscription', (req, res) => {
-    res.render('subscription');
+  app.get("/subscription", (req, res) => {
+    res.render("subscription");
   });
 
-  app.get('/cart', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/cart.html'));
+  app.get("/cart", (req, res) => {
+    res.render("cart");
   });
 
-  // blog route loads blog.html
-  app.get('/categories', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/categories.html'));
+  app.get("/checkout", (req, res) => {
+    res.render("checkout");
+  });
+
+  app.get("/confirmation", (req, res) => {
+    res.render("orderConfirmation");
+  });
+
+  app.get("/postAd", (req, res) => {
+    console.log("Clicked on the postAd");
+    res.render("postAd");
+  });
+
+  app.get("/login", (req, res) => {
+    res.render("login");
+  });
+  app.get("/contact", (req, res) => {
+    res.render("contact");
+  });
+
+  app.get("/categories", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/categories.html"));
   });
 };
